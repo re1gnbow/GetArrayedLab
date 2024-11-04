a39e418c353511066237b28711497ed9994095af
SalesTax <masseyas@mail.uc.edu>
2024-11-03 23:44:28 -0500

    02

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+import java.util.Scanner;
+
+class Lab_09_ArrayStuff {
+
+    public static void main(String[] args) {
+        // Task 1: Declare an array of type int named dataPoints
+        int[] dataPoints = new int[100];
+
+        // Print a confirmation message for Task 1
+        System.out.println("Array dataPoints has been declared with a length of " + dataPoints.length);
+    }
+}
